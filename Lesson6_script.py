60dc63a47de8abd150a3ed03fee0e129ce089288
GinBe <34943267+GinBe@users.noreply.github.com>
2021-07-15 15:50:31 +0800

    Update Lesson6_script.py

--- Lesson6_script.py
+++ Lesson6_script.py
@@ -84,3 +84,31 @@ for group in create_groups(range(32), 0):
 # print("\nCreating 0 groups...")
 # for group in create_groups(range(32), 0):
 #     print(list(group))
+
+"""读写文件
+以下是如何在 Python 中读写文件的方式。
+
+读取文件
+f = open('my_path/my_file.txt', 'r')
+file_data = f.read()
+f.close()
+首先使用内置函数 open 打开文件。需要文件路径字符串。open 函数会返回文件对象，它是一个 Python 对象，Python 通过该对象与文件本身交互。在此示例中，我们将此对象赋值给变量 f。
+你可以在 open 函数中指定可选参数。参数之一是打开文件时采用的模式。在此示例中，我们使用 r，即只读模式。这实际上是模式参数的默认值。
+使用 read 访问文件对象的内容。该 read 方法会接受文件中包含的文本并放入字符串中。在此示例中，我们将该方法返回的字符串赋值给变量 file_data。
+当我们处理完文件后，使用 close 方法释放该文件占用的系统资源。
+写入文件
+f = open('my_path/my_file.txt', 'w')
+f.write("Hello there!")
+f.close()
+以写入 ('w') 模式打开文件。如果文件不存在，Python 将为你创建一个文件。如果以写入模式打开现有文件，该文件中之前包含的所有内容将被删除。如果你打算向现有文件添加内容，但是不删除其中的内容，可以使用附加 ('a') 模式，而不是写入模式。
+使用 write 方法向文件中添加文本。
+操作完毕后，关闭文件。
+"""
+
+"""With
+Python 提供了一个特殊的语法，该语法会在你使用完文件后自动关闭该文件。
+
+with open('my_path/my_file.txt', 'r') as f:
+    file_data = f.read()
+该 with 关键字使你能够打开文件，对文件执行操作，并在缩进代码（在此示例中是读取文件）执行之后自动关闭文件。现在，我们不需要调用 f.close() 了！你只能在此缩进块中访问文件对象 f。
+"""
