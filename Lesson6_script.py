49fac57f2eebbac9a83687cc13ab71943aad08a1
GinBe <34943267+GinBe@users.noreply.github.com>
2021-07-19 20:13:18 +0800

    Update Lesson6_script.py

--- Lesson6_script.py
+++ Lesson6_script.py
@@ -178,17 +178,19 @@ word_list = []
 
 # We fill up the word_list from the `words.txt` file
 with open(word_file,'r') as words:
-	for line in words:
+    for line in words:
 		# remove white space and make everything lowercase
-		word = line.strip().lower()
+        word = line.strip().lower()
 		# don't include words that are too long or too short
-		if 3 < len(word) < 8:
-			word_list.append(word)
+        if 3 < len(word) < 8:
+            word_list.append(word)
 
 # TODO: Add your function generate_password below
 # It should return a string consisting of three random words 
 # concatenated together without spaces
 def generate_password():
+    return random.choice(word_list) + random.choice(word_list) + random.choice(word_list)
+	
     
 
 
