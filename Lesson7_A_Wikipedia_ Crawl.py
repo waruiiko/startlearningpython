9de00b9d3f1a9752901651413d63cff5991e7a1e
GinBe <34943267+GinBe@users.noreply.github.com>
2021-07-22 23:07:07 +0800

    Update Lesson7_A_Wikipedia_ Crawl.py

--- Lesson7_A_Wikipedia_ Crawl.py
+++ Lesson7_A_Wikipedia_ Crawl.py
@@ -1,5 +1,9 @@
 import requests
 response = requests.get('https://en.wikipedia.org/wiki/Dead_Parrot_sketch')
 print("======")
-print(response.text)
-print(type(response.text))
+# print(response.text)
+# print(type(response.text))
+html = response.text
+from bs4 import BeautifulSoup
+soup = BeautifulSoup(html, 'html.parser')
+print(soup.p)
