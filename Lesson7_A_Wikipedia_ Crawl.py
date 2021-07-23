fd6c238e70d93454d6bc3429ee2705a11a30a097
GinBe <34943267+GinBe@users.noreply.github.com>
2021-07-23 22:28:12 +0800

    Update Lesson7_A_Wikipedia_ Crawl.py

--- Lesson7_A_Wikipedia_ Crawl.py
+++ Lesson7_A_Wikipedia_ Crawl.py
@@ -1,9 +1,12 @@
+# pip3 install requests 安装requests
 import requests
 response = requests.get('https://en.wikipedia.org/wiki/Dead_Parrot_sketch')
 print("======")
 # print(response.text)
 # print(type(response.text))
 html = response.text
+
+# pip3 install beautifulsoup4 安装beautifulsoup4
 from bs4 import BeautifulSoup
 soup = BeautifulSoup(html, 'html.parser')
 print(soup.p)
